b39399c04b14fb7c664736e18b6c5db218151e45
aguillon <alexandre.guillon@cpe.fr>
2021-04-29 23:37:51 +0100

    (Feat) : Get tracks by asynchronous recursivity method to bypass Spotify limit

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,6 +1,6 @@
 const searchServices = require('../services/searchServices');
 
-const { searchArtist } = searchServices;
+const { searchArtist, searchTrackPool } = searchServices;
 
 exports.search = async (req, res, next) => {
     try {
@@ -18,6 +18,20 @@ exports.search = async (req, res, next) => {
                 res.status(400).json({
                     error: "Missing offset parameter."
                 })
+            }
+        }else if(req.query.tracks){
+            if(req.query.pool){
+                if(Number.isInteger(parseInt(req.query.pool)) && parseInt(req.query.pool) > 0 && parseInt(req.query.pool) < 1000){
+                    let result = await searchTrackPool(req.query.tracks, parseInt(req.query.pool));
+                    res.status(200).json(result);
+                }else{
+                    res.status(400).json({
+                        error: "Wrong offset parameter. It must be a positive integer between 1 and 1000."
+                    });
+                }
+            }else{
+                let result = await searchTrackPool(req.query.tracks);
+                res.status(200).json(result);
             }
         }else{
             res.status(400).json({
--- backend/services/searchServices.js
+++ backend/services/searchServices.js
@@ -1,27 +1,51 @@
 const spotifyService = require("./spotifyServices");
+const { getFormattedArtistslistAnswer, getFormattedTrackQuestion } = require('../utils/formattedUtils');
 
 exports.searchArtist = async (name, offset) => {
   try {
     let spotifyOffset = offset*5; // we want artists results by series of 5
     let results = await spotifyService.searchArtist(name, spotifyOffset);
-    let response = getFormattedSearchlistAnswer(results); // get a formatted answer from spotify results
+    let response = getFormattedArtistslistAnswer(results); // get a formatted answer from spotify results
     return response;
   } catch(e) {
     throw new Error(e.message)
   }
 }
 
-getFormattedSearchlistAnswer = results => {
-  let response = {};
-  response.total = results.artists.total;
-  response.artists = [];
-  results.artists.items.forEach(artist => {
-      let object = {};
-      object.id = artist.id;
-      object.imageUrl = artist.images[0] ? artist.images[0].url : null;
-      object.name = artist.name;
-      object.followers = artist.followers.total;
-      response.artists.push(object);
-  });
-  return response;
-}
+// https://medium.com/@wrj111/recursive-promises-in-nodejs-769d0e4c0cf9
+exports.searchTrackPool = async (name, pool = 10) => {
+  const trackItems = [];
+  if(pool < 1 || pool > 1000){ // pool value verification
+    throw new Error('Pool value must be between 1 and 1000.');
+  }
+  const searchLimitOffset = async (offset, pool) => {
+    if(pool > 50) {// spotify limits the number of results at 50 for each request
+      return spotifyService.searchTracks(name, offset, 50).then(tracks => {
+        trackItems.push(...tracks.tracks.items);
+        // if the list retrieved is below 50, then we can directly send the final answer to avoid useless next call to the Spotify API
+        return tracks.tracks.items.length == 50 ? searchLimitOffset(offset+50, pool-50) : trackItems; // asynchronous recursivity to fetch next results 
+      }).catch(err => {
+        throw new Error('Error while retrieving tracks : ' + err);
+      });
+    }else{
+      return spotifyService.searchTracks(name, offset, pool).then(tracks => {
+        trackItems.push(...tracks.tracks.items);
+        return trackItems; // final iteration where the number of results asked is below 50
+      }).catch(err => {
+        throw new Error('Error while retrieving tracks : ' + err);
+      });
+    }
+  };
+  const tracksResult = await searchLimitOffset(0, pool); // get the tracks
+  return {
+    size: tracksResult.length, // length of the tracks retrieved
+    tracks: tracksResult.map((track, index) => {
+      return {
+        page: Math.trunc(index/50) + 1, // 1 page contains 50 tracks
+        ...getFormattedTrackQuestion(track)
+      };
+    })
+  }
+};
+
+
